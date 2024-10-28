4d6e3a65b79ad07764e78f14e88e1b74bb5b2359
Chandan <hyeiamcosmos@gmail.com>
2024-10-28 11:11:19 +0530

    connection updates

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -7,6 +7,8 @@ export const sendToken = (user, statusCode, res, message) => {
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     httpOnly: true, // Set httpOnly to true
+    secure:true,
+    sameSite: 'None'
   };
 
   //console.log('options: ',options);
--- frontend/src/config/axiosConfig.js
+++ frontend/src/config/axiosConfig.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 const instance = axios.create({
   baseURL: import.meta.env.VITE_SERVER_URL, 
+  withCredentials: true,
 });
 
 export default instance;
